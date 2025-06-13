5142b9e7f05a0d839d99cd517955b6a072b18552
Harsh <harshchalwadi027@gmail.com>
2025-06-13 23:00:23 +0530

    updated the be and let make a fresh app

--- apps/backend/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    JWT_SECRET : "harsh@123"
-}
--- apps/frontend/src/components/Users.jsx
+++ apps/frontend/src/components/Users.jsx
@@ -22,10 +22,10 @@ export const Users = () => {
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
-        <div className="my-2">
+        <div className="py-4">
             <input onChange={(e)=>{
                 setFiltered(e.target.value)
-            }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+            }} type="text" placeholder="Search users..." className="w-full px-2 py-2 border rounded border-slate-200"></input>
         </div>
         <div>
             {users.map(user => <User user={user} />)}
@@ -35,15 +35,15 @@ export const Users = () => {
 
 function User({user}) {
     const navigate = useNavigate()
-    return <div className="flex justify-between">
-        <div className="flex">
+    return <div className="flex justify-between border p-4 mt-4">
+        <div className="flex  ">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center h-full text-xl">
+                <div className="flex uppercase flex-col justify-center h-full text-xl">
                     {user.firstName[0]}
                 </div>
             </div>
             <div className="flex flex-col justify-center h-ful">
-                <div>
+                <div className=" uppercase">
                     {user.firstName} {user.lastName}
                 </div>
             </div>
